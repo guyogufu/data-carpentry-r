3398a379a0fb670a9ca86765bab5ee691a74d001
guyogufu <guyo-duba.gufu@students.mq.edu.au>
2018-11-29 13:30:50 +1100

    Filter commit

--- dplyr.R
+++ dplyr.R
@@ -9,3 +9,9 @@ interviews <- read_csv("data/SAFI_clean.csv", na = "NULL")
 select(interviews, village, no_membrs, years_liv) # tidyverse does not need the double quotations if the column names are not r object
 
 select(interviews, village:rooms)
+
+# filter () function is used to subset rows
+
+filter(interviews, village == "God")
+
+filter(interviews, village == "God", rooms > 2)
