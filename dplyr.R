87c0da504713614a2fddaa105a401faccd078713
guyogufu <guyo-duba.gufu@students.mq.edu.au>
2018-11-29 13:19:41 +1100

    commit 2

--- dplyr.R
+++ dplyr.R
@@ -1,1 +1,11 @@
 #dplyr and tidyr
+
+library(tidyverse)
+
+interviews <- read_csv("data/SAFI_clean.csv", na = "NULL")
+
+#select a subset of columns using the selectet () function
+
+select(interviews, village, no_membrs, years_liv) # tidyverse does not need the double quotations if the column names are not r object
+
+select(interviews, village:rooms)
